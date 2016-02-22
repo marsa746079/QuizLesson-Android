ddec1d1eb9ec48838d5e837c96b7e4a076d4283d
marsa746079 <marsa746079@gmail.com>
2016-02-22 17:49:01 +0900

    Initial commit

---
+++ app/src/main/java/com/indigobell/quizlesson/common/QuizLessonConstants.java
@@ -0,0 +1,19 @@
+package com.indigobell.quizlesson.common;
+
+public class QuizLessonConstants {
+    private QuizLessonConstants(){}
+
+    public static final String FRAGMENT_TRUE_FALSE = "com.indigobell.quizlesson.fragment.TrueFalseFragment";
+    public static final String FRAGMENT_CHOICE = "com.indigobell.quizlesson.fragment.ChoiceFragment";
+    public static final String FRAGMENT_ASSOC = "com.indigobell.quizlesson.fragment.AssocFragment";
+    public static final String FRAGMENT_TYPING = "com.indigobell.quizlesson.fragment.TypingFragment";
+    public static final String FRAGMENT_EFFECT = "com.indigobell.quizlesson.fragment.EffectFragment";
+    public static final String FRAGMENT_CUBE = "com.indigobell.quizlesson.fragment.CubeFragment";
+    public static final String FRAGMENT_PANEL = "com.indigobell.quizlesson.fragment.PanelFragment";
+    public static final String FRAGMENT_SORT = "com.indigobell.quizlesson.fragment.SortFragment";
+    public static final String FRAGMENT_SLOT = "com.indigobell.quizlesson.fragment.SlotFragment";
+    public static final String FRAGMENT_MULTI = "com.indigobell.quizlesson.fragment.MultiFragment";
+    public static final String FRAGMENT_LINE = "com.indigobell.quizlesson.fragment.LineFragment";
+    public static final String FRAGMENT_ORDER = "com.indigobell.quizlesson.fragment.OrderFragment";
+
+}
